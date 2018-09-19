c334dd4302c45db4b28e65a4cb26a70079577544
barema4 <samrubarema6@gmail.com>
2018-09-19 11:06:47 -0700

    add fast-food-fast api endpoints files [#160609979]

---
+++ api/foodmodel.py
@@ -0,0 +1,41 @@
+from flask import request
+
+
+class OrderList:
+    def __init__(self):
+        self.orders = []
+
+    def get_all_orders(self):
+        return self.orders
+
+    def add_order(self, user_name, item_name, email, quantity, status, order_id):
+        xl = [order for order in self.orders]
+
+        id = len(xl) + 1
+
+        order = {
+            'User_name': user_name, 'item_name': item_name, 'email': email, 'item_quantity': quantity, 'status': status,
+            'id': id
+        }
+        self.orders.append(order)
+        return self.orders
+
+    def get_one_order(self, order_id):
+
+        for order in self.orders:
+            if order_id == order['id']:
+                return order
+        return ({'order': 'order_item doesnot exist'})
+
+    def update_order(self, order_id):
+        if order_id:
+            for order in self.orders:
+                if order_id == order['id']:
+                    item_json = request.get_json()
+                    status = item_json['status']
+                    order['status'] = status
+
+                    return order
+        else:
+            return "order item not found"
+
---
+++ api/views.py
@@ -0,0 +1,80 @@
+from flask import jsonify, request
+from flask.views import MethodView
+from foodmodel import OrderList
+import re
+
+orders = OrderList()
+
+
+class GetOrders(MethodView):
+
+    def get(self, order_id):
+        if order_id is None:
+            return jsonify({'orders': orders.get_all_orders()}), 200
+
+        else:
+
+            return jsonify({'orders': orders.get_one_order(order_id)}), 200
+
+    def post(self):
+        keys = ("user_name", "item_name", "email", "quantity", "status")
+
+        if not set(keys).issubset(set(request.json)):
+            return jsonify({'Blank space': 'Your request has Empty feilds'}), 400
+
+        if request.json['user_name'] == "":
+            return jsonify({'user_name': 'enter user_name'}), 400
+
+        if (' ' in request.json['user_name']) == True:
+            return jsonify({'message': 'user_name should not contain any spaces'}), 400
+
+        if request.json['item_name'] == "":
+            return jsonify({'item_name': 'enter item_name'}), 400
+
+        if (' ' in request.json['item_name']) == True:
+            return jsonify({'message': 'item_name should not contain any spaces'}), 400
+
+        if request.json['email'] == "":
+            return jsonify({'email': 'enter email'}), 400
+
+        if (' ' in request.json['email']) == True:
+            return jsonify({'message': 'email should not contain any spaces'}), 400
+        if request.json['quantity'] == "":
+            return jsonify({'quantity': 'please enter quantity'}), 400
+
+        if not isinstance(request.json['quantity'], int):
+            return jsonify({'message': 'enter quantity as an interger'}), 400
+
+        pattern = r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$"
+        if not re.match(pattern, request.json['email']):
+            return jsonify({'email': 'Enter way of your email'}), 400
+        if request.json['status'] == "":
+            return jsonify({'status': 'please enter status'}), 400
+
+        if not isinstance(request.json['status'], str):
+            return jsonify({'message': 'enter status as an string'}), 400
+
+        user_name = request.json['user_name']
+        item_name = request.json['item_name']
+        email = request.json['email']
+        item_quantity = request.json['quantity']
+        order_status = request.json['status']
+
+        id = 'id'
+        orders.add_order(user_name, item_name, email, item_quantity, order_status, id)
+        response_object = {
+            'orders': orders.__dict__
+        }
+        return jsonify(response_object), 201
+
+    def put(self, order_id):
+        if order_id:
+            orders.update_order(order_id)
+
+            response_object = {
+                'orders': orders.__dict__
+            }
+
+            return jsonify(response_object), 200
+        else:
+            return "order item not found"
